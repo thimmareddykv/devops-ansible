8b35e3abb98ca6b2c4cfae5cf12c539a75a23f7b
Michael Gaudet <michael.gaudet@wholefoods.com>
2015-12-22 17:31:32 -0600

    Used json to remove unicode markers on output Added comment with loop structure in case we want to get clever

--- python/ec2InventoryReport.py
+++ python/ec2InventoryReport.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from boto.ec2 import EC2Connection
- 
+import json
+
 csv_file = open('instances.csv','w+')
  
 def process_instance_list(connection):
@@ -18,8 +19,18 @@ def write_instances(instance):
 #    csv_file.write("%s,%s,%s,%s,%s,%s\n"%(instance.id,instance.public_dns_name,
 #                                          instance.state,instance.placement,instance.architecture, platform))
 #################################################################################################################
+
+    # Beginning of alternate method if we choose to break out tags:
+
+    # print 'test1\n'
+    # for (tagname, tagvalue) in instance.tags.items():
+    #   print tagname, tagvalue, '\n'
+    # print 'test2\n'
+
+    # csv_file.write("%s,%s,%s,%s,%s,%s,%s,%s,\"%s\"\n"%(instance.tags['Name'],instance.id,instance.public_dns_name,
+    #                                       instance.state,instance.placement,instance.architecture,instance.instance_type,instance.launch_time,str(instance.tags)))
     csv_file.write("%s,%s,%s,%s,%s,%s,%s,%s,\"%s\"\n"%(instance.tags['Name'],instance.id,instance.public_dns_name,
-                                          instance.state,instance.placement,instance.architecture,instance.instance_type,instance.launch_time,str(instance.tags)))
+                                          instance.state,instance.placement,instance.architecture,instance.instance_type,instance.launch_time,json.JSONEncoder().encode(instance.tags)))
     csv_file.flush()
  
 if __name__=="__main__":
